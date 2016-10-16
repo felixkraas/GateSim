c48e144244371810821be69595d036c36eecfc25
oneill011990 <felix@kraas-family.de>
2016-10-16 19:46:34 +0200

    Very first step for UI

---
+++ src/main/java/de/germanspacebuild/gatesim/gui/DisplayManager.java
@@ -0,0 +1,52 @@
+package de.germanspacebuild.gatesim.gui;
+
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.system.MemoryUtil;
+
+import static org.lwjgl.glfw.GLFW.*;
+
+/**
+ * Created by Felix on 11.10.2016
+ * for GateSim
+ *
+ * @author Felix
+ */
+public class DisplayManager {
+
+    public static long windowID;
+    private static GLFWErrorCallback errorCallback;
+
+    public static void createDisplay() {
+
+        glfwInit();
+
+        glfwSetErrorCallback(errorCallback = GLFWErrorCallback.createPrint(System.err));
+
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        windowID = glfwCreateWindow(1280, 720, "GateSim", MemoryUtil.NULL, MemoryUtil.NULL);
+
+        if (windowID == MemoryUtil.NULL) {
+            throw new RuntimeException("Failed to create windowID");
+        }
+
+        glfwMakeContextCurrent(windowID);
+        GL.createCapabilities();
+        glfwShowWindow(windowID);
+
+    }
+
+    public static void updateDisplay() {
+        glfwPollEvents();
+        glfwSwapBuffers(windowID);
+        glfwSwapInterval(1);
+    }
+
+    public static void closeDisplay() {
+        glfwDestroyWindow(windowID);
+    }
+
+}
